54b55e8cf722893f43e62b7b87dba35933e58af9
WhyK <windchimeyk@gmail.com>
2020-08-23 17:18:30 +0900

    update: config.jsを追加

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'VuePress + Netlify CMS Test',
+  description: 'VuePressとNetlify CMSが上手く接続できるかテストします',
+  dest: 'dist',
+  themeConfig: {
+    nav: [
+      { text: 'Guide', link: '/guide/' },
+      { text: 'GitHub', link: 'https://github.com/windchime-yk/learn-vuepress-netlifycms' },
+    ],
+  },
+}
---
+++ docs/index.md
@@ -0,0 +1,6 @@
+---
+title: TOP
+---
+
+# Learn VuePress & NetlifyCMS
+今回はVuePressとNetlifyCMSを使って、更新ができるか試していく。
